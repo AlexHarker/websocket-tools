b36100c2a237e803b7b4a9776a1047ff24d2320a
Alex Harker <alexharker@SAHLM129770.local>
2024-10-05 07:44:33 +0100

    Doxygen Tidy-Up #1 (requires check)

--- include/common/ws_client_base.hpp
+++ include/common/ws_client_base.hpp
@@ -4,27 +4,26 @@
 
 #include "ws_base.hpp"
 
-// A base class for websocket clients
 /**
  * @brief A base class template for WebSocket clients.
  *
- * This class serves as a base for WebSocket client implementations, inheriting from `ws_base`.
- * It provides a static method to create a WebSocket client instance.
+ * This class serves as a base for WebSocket client implementations, inheriting from `ws_base`. It provides a static 
+ * method to create a WebSocket client instance.
  *
  * @tparam T The type of the WebSocket client.
  * @tparam U The type of the WebSocket client handler.
  */
+
 template <class T, class U>
 class ws_client_base : public ws_base<T, U>
 {
 public:
     
-    // Create
     /**
      * @brief Creates a WebSocket client instance.
      *
-     * This static method creates and returns a WebSocket client instance by delegating
-     * to the `ws_base` class's `create` method.
+     * This static method creates and returns a WebSocket client instance by delegating to the `ws_base` class's 
+     * `create` method.
      *
      * @tparam handlers A constant reference to the WebSocket client handlers.
      * @param host The host address of the WebSocket server.
@@ -33,6 +32,7 @@ public:
      * @param owner The WebSocket client owner, containing the necessary handlers.
      * @return T* A pointer to the newly created WebSocket client instance.
      */
+    
     template <const ws_client_handlers& handlers>
     static T *create(const char *host,
                      uint16_t port,
